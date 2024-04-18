68d84e63345f2e049884fc6a1c8c708bdf7a7d67
symbicort <jeongwonlee0@gmail.com>
2024-04-18 16:28:41 +0900

    folder name change

--- finalFront/src/components/TabContent.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-interface TabContentProps {
-  content: ReactNode;
-}
-
-export default function TabContent({ content }: TabContentProps) {
-  return <div>{content}</div>;
-}
