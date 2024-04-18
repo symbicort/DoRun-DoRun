ed7bca80dbc5a65026251da7bdf3bec8eea4649a
symbicort <jeongwonlee0@gmail.com>
2024-04-18 16:10:13 +0900

    front init

---
+++ finalFront/src/main.tsx
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import App from './App.tsx';
+import store from './store/store.ts';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
